362de4ca197cd1a192a5e5be5ef0778507cb34fc
Arthur Barbosa <arthur.bos@hotmail.com>
2022-07-25 17:57:48 -0300

    add assets

---
+++ lib/app/core/mock_data/mock_data.dart
@@ -0,0 +1,53 @@
+List<Map<String, dynamic>> listProductMock = [
+  {
+    "id": "01",
+    "short_title": "Lenovo - IdeaPad L340 15 Gaming",
+    "image": [
+      "assets/item_photos/lenovo_idealpad/lenovo1.png",
+      "assets/item_photos/lenovo_idealpad/lenovo2.png",
+      "assets/item_photos/lenovo_idealpad/lenovo3.png",
+    ],
+    "price": "\$ 717.80",
+    "itemInCart": 1,
+    "title":
+        "Notebook Gamer Lenovo Ideapad L340, Processador Intel Core I5 8gb 256gb Ssd Tela 15.6, Preto",
+  },
+  {
+    "id": "02",
+    "short_title": "Lenovo 15.6\" ThinkPad P15s Gen 1",
+    "image": [
+      "assets/item_photos/lenovo_thinkpad/lenovo1.png",
+      "assets/item_photos/lenovo_thinkpad/lenovo2.png",
+      "assets/item_photos/lenovo_thinkpad/lenovo3.png",
+    ],
+    "price": "\$ 1,519.99",
+    "itemInCart": 1,
+    "title":
+        "Lenovo 15.6\" ThinkPad P15s Gen 1 Laptop, Intel Core i7-10510U Quad-Core, 16GB DDR4 RAM, 512GB SSD, NVIDIA Quadro P520, Windows 10 Pro (20T4001VUS)",
+  },
+  {
+    "id": "03",
+    "short_title": "Notebook Lenovo 2 em 1",
+    "image": [
+      "assets/items_photos/lenovo/lenovo.png",
+    ],
+    "price": "\$ 4,699.00",
+    "itemInCart": 0,
+    "title":
+        "Notebook Gamer Lenovo Ideapad L340, Processador Intel Core I5 8gb 256gb Ssd Tela 15.6, Preto",
+  },
+];
+
+String descriptionMock = """
+1.8 GHz Intel Core i7-10510U Quad-Core Processor
+16GB of DDR4 RAM | 512GB SSD
+15.6" 1920 x 1080 IPS Display
+NVIDIA Quadro P520
+Windows 10 Pro 64-Bit Edition
+1.8 GHz Intel Core i7-10510U Quad-Core Processor
+16GB of DDR4 RAM | 512GB SSD
+15.6" 1920 x 1080 IPS Display
+NVIDIA Quadro P520
+""";
+
+int mockItemInCart = 2;
